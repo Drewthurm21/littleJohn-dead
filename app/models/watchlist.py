101244a5c8ef0a853203df265b4488a430c90242
Drewthurm21 <drewthurm21@gmail.com>
2021-06-28 17:37:28 -0500

    added to_dict methods on watchlist and watchlist_stock models

--- app/models/watchlist.py
+++ app/models/watchlist.py
@@ -8,5 +8,12 @@ class Watchlist(db.Model):
     name = db.Column(db.String(60), nullable=False)
     user_id = db.Column(db.Integer, ForeignKey='users', nullable=False)
 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'user_id': self.user_id
+        }
+
     user = relationship('User', back_populates='watchlists')
     stocks = relationship('Watchlist_stock', back_populates='watchlists')
--- app/models/watchlist_stocks.py
+++ app/models/watchlist_stocks.py
@@ -9,4 +9,11 @@ class Watchlist_stock(db.Model):
     watchlist_id = db.Column(db.Integer, ForeignKey='watchlists', nullable = False)
     ticker = db.Column(db.String(10), nullable = False, unique = True)
 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'watchlist_id': self.watchlist_id,
+            'ticker': self.ticker
+        }
+
     watchlist = relationship('Watchlist', back_populates='watchlist_stocks')
