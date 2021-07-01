c157d2fba28da5b4a5a57d4b91c594d75d7ded90
Drewthurm21 <drewthurm21@gmail.com>
2021-07-01 13:17:25 -0500

    added get route for user watchlists

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import User, Portfolio
+from app.models import User, Portfolio, Watchlist
 
 user_routes = Blueprint('users', __name__)
 
@@ -17,3 +17,10 @@ def user_portfolio(id):
     portfolios = Portfolio.query.filter_by(owner_id=id)
     portfolio_list = [portfolio.to_dict() for portfolio in portfolios]
     return {"portfolios": portfolio_list}
+
+
+@user_routes.route('/<int:id>/watchlists')
+def user_watchlists():
+    watchlists = Watchlist.query.filter_by(user_id=id)
+    watchlist_list = [watchlist.to_dict() for watchlist in watchlists]
+    return {"watchlists": watchlist_list}
