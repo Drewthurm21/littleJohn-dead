d1aa93602ebd6e5258f20e9e6d2ac08ecb82937c
Drewthurm21 <drewthurm21@gmail.com>
2021-08-10 17:09:04 -0400

    adding socket file

---
+++ app/web_sockets.py
@@ -0,0 +1,11 @@
+from flask_socketio import SocketIO
+
+if os.environ.get(FLASK_ENV) == "production"
+    origins = [
+        'http://myURLhere',
+        'https://myURLhere'
+    ]
+else:
+    origins = '*'
+
+socketio = SocketIO(cors_allowed_origins=origins)
